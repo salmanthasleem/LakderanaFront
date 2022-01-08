4b414aebe1934c817e58a79c8f203de2934922ea
Salman Thasleem <salmanthasleem@gmail.com>
2022-01-08 16:02:36 +0530

    first build

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://192.168.43.85:3001/api/'
+    baseURL: 'https://lakderana.as.r.appspot.com/api'
 })
 
 export default api
